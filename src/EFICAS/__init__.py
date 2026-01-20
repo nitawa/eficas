d4278e686093d8c73fc2d9fb0b3bd4a98c287487
PASCALE NOYRET <pascale.noyret@edf.fr>
2026-01-20 17:43:08 +0100

    chgt copyright

--- src/EFICAS/__init__.py
+++ src/EFICAS/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: iso-8859-1 -*-
-# Copyright (C) 2006-2023  CEA, EDF
+# Copyright (C) 2001-2026  CEA, EDF
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
